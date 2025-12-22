2b6b651172c0aa38afba5f17216975ac5964276e
Catherine <>
2025-12-21 22:02:11 -0500

    Adding basic graphical things

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package finalproject;
-
 /**
  *
  * @author Notak
@@ -14,7 +13,7 @@ public class FinalProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MySketch.main("finalproject.MySketch");
     }
     
 }
--- src/finalproject/MySketch.java
+++ src/finalproject/MySketch.java
@@ -12,6 +12,49 @@ import processing.core.PApplet;
 
 public class MySketch extends PApplet {
 
+    private Player player;
+    private boolean showInfo = false;
     
+    public void settings(){
+        size(1500, 800);
+    }
+    
+    public void setup(){
+        background(255);
+        player = new Player(this, 0, 0, "Player", "images/idle/player1.png");
+    }
+    
+    public void mousePressed(){
+        if(player.isClicked(mouseX,mouseY)){
+            showInfo = !showInfo;
+        }else{
+            showInfo = false;
+        }
+    }
+    
+    public void draw(){
+        background(255);
+        int dx = 0;
+        int dy = 0;
+        
+        if (keyPressed){
+            if (key == 'a') dx = -3;
+            else if (key == 'd') dx = 3;
+            else if (key == 'w') dy = -3;
+            else if (key == 's') dy = 3;
+        }
+        
+        player.move(dx,dy);
+        player.draw();
+        
+        //Collide Checker
+        noFill();
+        stroke(255,0,0);
+        rect(player.x,player.y, player.getWidth(), player.getHeight());
+        
+        if (showInfo){
+            player.displayInfo(this);
+        }
+    }
     
 }
